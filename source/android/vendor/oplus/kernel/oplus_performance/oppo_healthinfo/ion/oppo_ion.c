676e3a63ad25d750ac47fd44d27a0e39ace585d0
pswbuild <pswbuild@oppo.com>
2026-08-14 17:08:28 +0800

    Synchronize code for OPPO PAFM00_11_H.15 Based on QCOM release TAG: platform/vendor/opensource/audio-kernel:AU_LINUX_ANDROID_LA.UM.9.3.R1.11.00.00.807.023_r1.0.r1_00050.1a platform/vendor/qcom-opensource/data-kernel:AU_LINUX_ANDROID_LA.UM.9.3.R1.11.00.00.807.023_r1.0.r1_00050.1a platform/vendor/qcom-opensource/wlan/fw-api:AU_LINUX_ANDROID_LA.UM.9.3.R1.11.00.00.807.021_r1.0.r1_00040.1 platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn:AU_LINUX_ANDROID_LA.UM.9.3.R1.11.00.00.807.021_r1.0.r1_00040.1 platform/vendor/qcom-opensource/wlan/qcacld-3.0:AU_LINUX_ANDROID_LA.UM.9.3.R1.11.00.00.807.021_r1.0.r1_00040.1 platform/vendor/qcom-opensource/wlan/utils/sigma-dut:AU_LINUX_ANDROID_LA.UM.9.3.R1.11.00.00.807.023_r1.0.r1_00050.1a

---
+++ source/android/vendor/oplus/kernel/oplus_performance/oppo_healthinfo/ion/oppo_ion.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2020 Oplus. All rights reserved.
+ */
+
+#include <../drivers/staging/android/ion/msm_ion_priv.h>
+
+atomic_long_t ion_total_size;
+bool ion_cnt_enable = true;
+
+unsigned long ion_total(void)
+{
+	if (!ion_cnt_enable)
+		return 0;
+	return (unsigned long)atomic_long_read(&ion_total_size);
+}
